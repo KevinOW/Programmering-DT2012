3ec67413b111142f58423fcde3980ce5907fae53
Kevin <43371558+KevinOW@users.noreply.github.com>
2022-11-03 00:45:03 +0100

    Week 1 - Correct version

---
+++ Uppgifter-Vecka-1/src/E10.java
@@ -0,0 +1,33 @@
+/*
+
+Add code to print
+five uniform random values between 0 and 1,
+their average value and
+their minimum and maximum value.
+
+Use Math.random(), Math.min(), and Math.max().
+
+*/
+public class E10{
+
+  public static void main(String[] args) {
+
+    // Your code here.
+
+      int n = 5;
+      double value1 = Math.random();
+      double value2 = Math.random();
+      double value3 = Math.random();
+      double value4 = Math.random();
+      double value5 = Math.random();
+
+      double min = Math.min(value1, Math.min(value2, Math.min(value3,Math.min(value4, value5))));
+      double max = Math.max(value1, Math.max(value2, Math.max(value3,Math.max(value4, value5))));
+      double avg = (value1 + value2 + value3 + value4 + value5 / n);
+      System.out.println("Min: " + min);
+      System.out.println("Max: " + max);
+      System.out.println("Avg.: " + avg);
+
+  }
+
+}
