cf77bd491b9042a2a70b7d358704886b8e693b19
Kevin <43371558+KevinOW@users.noreply.github.com>
2022-11-04 02:08:25 +0100

    E11 - Reverse Fix #2

--- Uppgifter-Vecka-1/src/E11.java
+++ Uppgifter-Vecka-1/src/E11.java
@@ -17,11 +17,9 @@ public class E11{
     int y = Integer.parseInt(args[1]);
     int z = Integer.parseInt(args[2]);
 
-    int a = Math.min(x,y);
-    int b = Math.max(x,y);
-    int min = Math.min(a,z);
-    int max = Math.max(b,z);
-    int median = x + y + z - min - max;
+    int min = Math.min(Math.min(x, y), z);
+    int max = Math.max(Math.max(x, y), z);
+    int median = (x + y + z) - (min + max);
 
     System.out.println(min);
     System.out.println(median);
