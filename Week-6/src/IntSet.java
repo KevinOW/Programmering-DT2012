362ca6fbe978de7ce90b34befdbb08b2c57440a8
Kevin <43371558+KevinOW@users.noreply.github.com>
2022-12-07 15:49:42 +0100

    intSet

--- Week-6/src/IntSet.java
+++ Week-6/src/IntSet.java
@@ -25,37 +25,72 @@
 
 */
 
-public class IntSet{
+*/
+
+public class IntSet {
   /*
   Your code here: instance variables
   */
+    boolean[] array;
+    int bound;
 
-  public IntSet(int n){
-    // Your code here: constructor for the empty set of positive integers
-    // between 0 and n.
-  }
+    public IntSet(int n){
+        // Your code here: constructor for the empty set of positive integers
+        // between 0 and n.
+        array = new boolean[n+1];
+        bound = array.length - 1;
+    }
 
-  public IntSet(int n, int[] values){
-    // Your code here: constructor for the set of positive integers
-    // between 0 and n
-    // with the members  provided by the array values.
-  }
+    public IntSet(int n, int[] values){
+        // Your code here: constructor for the set of positive integers
+        // between 0 and n
+        // with the members  provided by the array values.
+        array = new boolean[n+1];
+        for (int i = 0; i < values.length; i++) {
+            array[values[i]] = true;
+        }
+        bound = array.length - 1;
+    }
 
-  public boolean has(int x){
-    // Your code here: tests whether the integer x belongs to this set.
-    return false;
-  }
+    public boolean has(int x){
+        if (x > 0 && x <= bound) {
+            return array[x];
+        }
+        return false;
+        // Your code here: tests whether the integer x belongs to this set.
+    }
 
-  public void addValue(int x){
-    // Your code here: adds the element x to this set.
-  }
+    public void addValue(int x){
+        if (x > 0 && x <= bound) {
+            array[x] = true;
+        }
 
-  public String toString(){
-    // Your code here: returns a String version of this set.
-    return "";
-  }
+        // Your code here: adds the element x to this set.
+    }
 
-  public static void main(String[] args){
-    // Your code here: a small test of operations of IntSet.
-  }
+    public String toString(){
+        String string = "{ ";
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]) {
+                string += Integer.toString(i);
+                string += " ";
+            }
+        }
+        string += "}";
+        // Your code here: returns a String version of this set.
+        return string;
+    }
+
+    public static void main(String[] args){
+        IntSet intSet = new IntSet(5);
+        intSet.addValue(2);
+        intSet.addValue(4);
+        intSet.addValue(6);
+        System.out.println("intSet contains 2: " + intSet.has(2));
+        System.out.println("intSet contains 1: " + intSet.has(1));
+        System.out.println("intSet contains 6: " + intSet.has(6));
+        System.out.println(intSet);
+
+        // Your code here: a small test of operations of IntSet.
+    }
 }
